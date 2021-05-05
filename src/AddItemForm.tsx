2b8674eeb8a4958355e1fa7076a225d355c6d889
igorlitvintsev <litvincevi@mail.ru>
2021-05-05 18:40:43 +0300

    12.Storybook & Snapshot

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -3,7 +3,7 @@ import {IconButton, TextField} from "@material-ui/core";
 import {AddBox} from "@material-ui/icons";
 
 
-type AddItemFormPropsType = {
+export type AddItemFormPropsType = {
     addItem: (title: string) => void
 }
 
